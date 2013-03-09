f45bad179d6a0666d50d4dcd167700629a485251
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-09 22:31:57 +0000

    Drink add interface

--- src/uk/co/fuuzetsu/wetting/AddActivity.java
+++ src/uk/co/fuuzetsu/wetting/AddActivity.java
@@ -2,42 +2,50 @@ package uk.co.fuuzetsu.wetting;
 
 import android.app.*;
 import android.content.*;
+import android.util.*;
 import android.os.*;
+import android.text.*;
 import android.view.*;
+import android.view.View.*;
 import android.widget.*;
 
 
 public class AddActivity extends Activity {
 
-
+	private final String TAG = "AddActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add);
 
-		/* Disable the button on start up */
-		Button save = (Button) findViewById(R.id.buttonSaveDrink);
+		/* Disable the appropriate items */
+		final Button save = (Button) findViewById(R.id.buttonSaveDrink);
+		final CheckBox fizzyCheck = (CheckBox) findViewById(R.id.fizzyCheckbox);
 		save.setEnabled(false);
 
-		EditText drinkInput = (EditText) findViewById(R.id.newDrinkTextbox);
-		Spinner drinkSpinner = (Spinner) findViewById(R.id.oldDrinkSpinner);
+		final EditText drinkInput = (EditText) findViewById(R.id.newDrinkTextbox);
+		final Spinner drinkSpinner = (Spinner) findViewById(R.id.oldDrinkSpinner);
 
-		drinkInput.setOnFocusChangeListener(new OnFocusChangeListener() {
+		Log.d(TAG, "In after last init");
+
+		drinkInput.addTextChangedListener(new TextWatcher() {
 
 				@Override
-				public void onFocusChange(View v, boolean hasFocus) {
-					if (drinkInput.getText().toString().length() == 0) {
-						if (drinkSpinner.getSelectedItem().toString().length() == 0) {
+				public void afterTextChanged(Editable s) {
+					if (drinkInput.length() == 0) {
+						TextView drinkSpinnerTV = (TextView) drinkSpinner.getSelectedItem();
+						if (drinkSpinnerTV == null || drinkSpinnerTV.toString().length() == 0) {
 							drinkInput.setEnabled(true);
 							drinkSpinner.setEnabled(true);
 							save.setEnabled(false);
+							fizzyCheck.setEnabled(false);
 						}
 						else {
 							drinkSpinner.setEnabled(false);
 							save.setEnabled(true);
+							fizzyCheck.setEnabled(true);
 						}
-
 					}
 					else {
 						drinkSpinner.setEnabled(false);
@@ -45,6 +53,8 @@ public class AddActivity extends Activity {
 					}
 				}
 
+				public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+				public void onTextChanged(CharSequence s, int start, int before, int count){}
 			});
 
 
