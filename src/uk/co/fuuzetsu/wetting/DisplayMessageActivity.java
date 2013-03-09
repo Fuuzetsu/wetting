c95defb9f8f4b5a3bc2ef6ca242e30c478a3c38d
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-09 17:54:41 +0000

    Remove code using newer API causing a crash

--- src/uk/co/fuuzetsu/wetting/DisplayMessageActivity.java
+++ src/uk/co/fuuzetsu/wetting/DisplayMessageActivity.java
@@ -22,10 +22,5 @@ public class DisplayMessageActivity extends Activity {
 
 		setContentView(textView);
 
-        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // Show the Up button in the action bar.
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-        }
     }
 }
