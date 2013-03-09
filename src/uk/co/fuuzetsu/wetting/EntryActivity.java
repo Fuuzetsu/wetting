6a2f0f16df66d7065e3de719001f731bc365f5b3
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-09 18:24:18 +0000

    Entry activity class

---
+++ src/uk/co/fuuzetsu/wetting/EntryActivity.java
@@ -0,0 +1,18 @@
+package uk.co.fuuzetsu.wetting;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+
+public class EntryActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+    }
+
+}
