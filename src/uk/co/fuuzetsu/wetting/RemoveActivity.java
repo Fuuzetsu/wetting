7df9ec16380787f01e934c6c7cf16d93c284fb76
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-09 21:09:32 +0000

    layout + vals for remove act

---
+++ src/uk/co/fuuzetsu/wetting/RemoveActivity.java
@@ -0,0 +1,17 @@
+package uk.co.fuuzetsu.wetting;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+
+public class RemoveActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remove);
+    }
+}
