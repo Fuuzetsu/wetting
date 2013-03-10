9eea71a14afa7575929c9a0ccdea604c84a7cc32
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-10 05:51:40 +0000

    properly compare the days

--- src/uk/co/fuuzetsu/wetting/EntryActivity.java
+++ src/uk/co/fuuzetsu/wetting/EntryActivity.java
@@ -28,33 +28,41 @@ public class EntryActivity extends Activity {
         setContentView(R.layout.entry);
 
 		this.diary = loadDiary();
+		Calendar c = Calendar.getInstance();
+		Date time = new Date(c.getTimeInMillis());
+		changeDay(time);
+
+    }
+
+	public void changeDay(Date time) {
 
 		TextView tv = (TextView) findViewById(R.id.dateLabel);
 
+		SimpleDateFormat df = new SimpleDateFormat("dd MMMM yyyy");
+		Log.v(TAG, "Changing day to " + df.format(time));
 
-		Calendar c = Calendar.getInstance();
-		SimpleDateFormat dfDate_day= new SimpleDateFormat("dd MMMM yyyy");
-		Date time = c.getTimeInMillis();
-		tv.setText(dfDate_day.format(time));
+		tv.setText(df.format(time));
 
 		List<String> entries = populateList(this.diary, time);
 
 		ListView lv = (ListView) findViewById(R.id.entryList);
 		ArrayAdapter adptr = new ArrayAdapter(this, android.R.layout.simple_list_item_1, entries);
 		lv.setAdapter(adptr);
-
-    }
+	}
 
 	public List<String> populateList(DrinkDiary d, Date time) {
-		SimpleDateFormat df= new SimpleDateFormat("HH:MM");
+		SimpleDateFormat df = new SimpleDateFormat("HH:MM");
 		SimpleDateFormat dayFormat = new SimpleDateFormat("dd/MM/yyyy");
 		List<String> l = new ArrayList<String>();
 
 		for (Map.Entry<Long, Either<Drink, Toilet>> entry : d.getActivities().entrySet()) {
 			Long dt = entry.getKey();
 			String date = df.format(new Date(dt));
-			String dateDay = dayFormat(new Date(dt));
-			if (date.equals(dateDay)) {
+			String dateDay = dayFormat.format(new Date(dt));
+			Log.v(TAG, "date: " + date);
+			Log.v(TAG, "dateDay: " + dateDay);
+
+			if (dayFormat.format(time).equals(dateDay)) {
 				Either<Drink, Toilet> v = entry.getValue();
 				if (v.isLeft())
 					l.add(date + " | " + v.getLeft().getName());
