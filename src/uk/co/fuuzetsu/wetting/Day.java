43614904173a7cdfe6a0fbfeb16621a495cc5720
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-03-10 09:29:23 +0000

    Fix Twey's Day class.

--- src/uk/co/fuuzetsu/wetting/Day.java
+++ src/uk/co/fuuzetsu/wetting/Day.java
@@ -1,6 +1,15 @@
 package uk.co.fuuzetsu.wetting;
 
+import java.util.*;
+
 public class Day {
-    boolean wet;
-    java.util.Map<java.util.Date, Either<Drink, Toilet>> events;
+    private Boolean wet;
+	private Date start;
+    private Map<Date, Either<Drink, Toilet>> events;
+
+	public Day(Boolean wet, Date start, Map<Date, Either<Drink, Toilet>> events) {
+		this.wet = wet;
+		this.start = start;
+		this.events = events;
+	}
 }
